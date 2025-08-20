99fdd208196ca53c881c03490ec32a8f8e934fab
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-08-20 01:36:12 -0700

    Add Main

--- src/main/java/aut/ap/model/User.java
+++ src/main/java/aut/ap/model/User.java
@@ -5,57 +5,33 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-<<<<<<< Updated upstream
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-
-    public User() {}
-
-=======
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false, unique = true, length = 150)
     private String email;
 
+    @Column(nullable = false, length = 100)
     private String password;
 
-    // ⚡ سازنده بدون پارامتر (لازم برای Hibernate)
-    public User() {}
+//null constructor for Hibernate
+public User() {
+    }
 
-    // ⚡ سازنده کامل (لازم برای UserService)
->>>>>>> Stashed changes
-    public User(String name, String email, String password) {
+//constructor for sign up
+public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
-<<<<<<< Updated upstream
-    public Integer getId() { return id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-=======
-    // --- Getters & Setters ---
-    public int getId() {
+// getter & setter
+public int getId() {
         return id;
     }
 
@@ -86,5 +62,4 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
->>>>>>> Stashed changes
 }
