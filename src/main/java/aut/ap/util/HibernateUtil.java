fda4388046ff477a4c177a473eb1b5ec9a338230
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-08-20 01:23:16 -0700

    Add hibernate

--- src/main/java/aut/ap/util/HibernateUtil.java
+++ src/main/java/aut/ap/util/HibernateUtil.java
@@ -1,4 +1,29 @@
 package aut.ap.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // تنظیمات از hibernate.cfg.xml خونده میشه
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("❌ Initial SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    // برای استفاده در سرویس‌ها
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    // بستن SessionFactory وقتی برنامه تموم میشه
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
 }
