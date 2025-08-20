deb1e74fce719953e998d228b74d12c381a5125b
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-08-19 17:59:50 -0700

    Add Hibernate for connecting to database

--- src/main/java/aut/ap/util/HibernateUtil.java
+++ src/main/java/aut/ap/util/HibernateUtil.java
@@ -1,4 +1,28 @@
 package aut.ap.util;
 
+import aut.ap.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            configuration.addAnnotatedClass(User.class);
+            return configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
 }
